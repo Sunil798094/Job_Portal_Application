f3b927cb75db907354d2098db499d345972cb602
Sunil Chouhan <sunil.chouhan@cubastion.com>
2024-01-04 15:04:44 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'home', component : HomeComponent},
+ {path:'login', component : LoginComponent},
+ {path:'registration',component: RegistrationComponent},
+ {path:'jobs', component: JobsComponent},
+ {path: 'job-details', component:JobDetailsComponent},
+ 
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
